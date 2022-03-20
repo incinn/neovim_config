5d3c25828d23048425015f93ded9ca8d5e283778
Barnz <hi@barnz.dev>
2022-03-20 21:13:13 +0000

    add nvimtree and icon pack

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -43,6 +43,8 @@ return packer.startup(function(use)
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and nvim-treesitter
   use "numToStr/Comment.nvim" -- Easily comment stuff
+  use "kyazdani42/nvim-web-devicons"
+  use "kyazdani42/nvim-tree.lua"
 
   -- colorschemes
   use "sainnhe/sonokai"
