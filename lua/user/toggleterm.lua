de8962baa370bf46e299e1a69cc3dd57666f0d29
barnz <hi@barnz.dev>
2022-05-07 13:18:08 +0100

    set default shell to zsh

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -15,8 +15,8 @@ toggleterm.setup({
 	persist_size = true,
 	direction = "float",
 	close_on_exit = true,
-	shell = vim.o.shell,
-	float_opts = {
+  shell = "/usr/bin/zsh",
+  float_opts = {
 		border = "curved",
 		winblend = 0,
 		highlights = {
