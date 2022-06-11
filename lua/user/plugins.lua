a2331174c58a613766ee4b58e350270763c2bec3
barnz <hi@barnz.dev>
2022-06-11 12:55:20 +0100

    install vim-go plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -74,6 +74,9 @@ return packer.startup(function(use)
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+  
+  -- code
+  use "fatih/vim-go" -- go plugin
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
